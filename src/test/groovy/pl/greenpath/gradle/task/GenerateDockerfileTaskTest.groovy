4023f188ab3c7c791f5421dd5b154cbe936393a7
Jan Rotter <jan.rotter@gmail.com>
2015-09-09 00:38:17 +0200

    Refactor GenerateDockerfileTask tests.

--- src/test/groovy/pl/greenpath/gradle/task/GenerateDockerfileTaskTest.groovy
+++ src/test/groovy/pl/greenpath/gradle/task/GenerateDockerfileTaskTest.groovy
@@ -1,6 +1,7 @@
 package pl.greenpath.gradle.task
 
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
 import pl.greenpath.gradle.DockerPlugin
 import pl.greenpath.gradle.extension.DockerExtension
@@ -8,28 +9,51 @@ import spock.lang.Specification
 
 class GenerateDockerfileTaskTest extends Specification {
 
-  static final String TASK_NAME = 'generateDockerfile'
+  static final String GENERATE_DOCKERFILE_TASK_NAME = 'generateDockerfile'
+
+  static final String TEST_PROJECT_NAME = 'testProject'
 
   Project rootProject
 
+  Task generateDockerfileTask
+
   def setup() {
-    rootProject = ProjectBuilder.builder().withName('testProject').build()
+    rootProject = createTestProject()
+
+    applyDockerPlugin(rootProject)
+
+    generateDockerfileTask = findTask(rootProject, GENERATE_DOCKERFILE_TASK_NAME)
+
+    createTemporaryBuildDirForProject()
+  }
+
+  private applyDockerPlugin(Project rootProject) {
     def plugin = new DockerPlugin()
     plugin.apply(rootProject)
   }
 
+  Task findTask(Project project, String taskName) {
+    project.getTasksByName(taskName, false).first()
+  }
+
+  Project createTestProject() {
+    rootProject = ProjectBuilder.builder().withName(TEST_PROJECT_NAME).build()
+    rootProject
+  }
+
+  void createTemporaryBuildDirForProject() {
+    rootProject.buildDir = File.createTempDir()
+  }
+
   def "should generate Dockerfile for Spring Boot microservice when corresponding template is given"() {
     given:
-    def tempDir = File.createTempDir()
     rootProject.version = '1.1'
     rootProject.extensions['docker'].port 8082
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.dockerfile.with DockerExtension.microserviceTemplate
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''FROM java:8
                                      |EXPOSE 8082
@@ -40,17 +64,14 @@ class GenerateDockerfileTaskTest extends Specification {
 
   def "should be able to redefine parameters after applying template"() {
     given:
-    def tempDir = File.createTempDir()
     rootProject.version = '1.1'
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.port 8082
     rootProject.extensions.docker.dockerfile.template DockerExtension.microserviceTemplate
     rootProject.extensions.docker.dockerfile.add('testing', '.')
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''FROM java:8
                                      |EXPOSE 8082
@@ -61,46 +82,37 @@ class GenerateDockerfileTaskTest extends Specification {
   }
 
   def "should generate Dockerfile with user defined base image when defined in extension"() {
-    def tempDir = File.createTempDir()
     rootProject.extensions.docker.dockerfile.from 'postgres:9.4'
     rootProject.version = '1.2'
-    rootProject.buildDir = tempDir
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''FROM postgres:9.4
                                      |'''.stripMargin()
   }
 
   def "should generate Dockerfile with port defined in docker extension"() {
-    def tempDir = File.createTempDir()
     rootProject.version = '1.2'
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.port 8080
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''EXPOSE 8080
                                      |'''.stripMargin()
   }
 
   def "should replace content of file if already exists"() {
-    def tempDir = File.createTempDir()
     rootProject.version = '1.2'
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.port 8080
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
-    task.executeTask()
+    generateDockerfileTask.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''EXPOSE 8080
                                      |'''.stripMargin()
@@ -108,36 +120,31 @@ class GenerateDockerfileTaskTest extends Specification {
 
   def "should skip dockerfile generation when generateDockerfile is set to false"() {
     given:
-    def tempDir = File.createTempDir()
     rootProject.version = '1.1'
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.port 8082
     rootProject.extensions.docker.dockerfile.template DockerExtension.microserviceTemplate
     rootProject.extensions.docker.dockerfile.add('testing', '.')
     rootProject.extensions.docker.generateDockerfile false
-    def task = rootProject.getTasksByName(TASK_NAME, false)[0]
     when:
-    task.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     !dockerfile.exists()
   }
 
 
   def "should generate Dockerfile based on string representation passed as 'dockerfile' argument "() {
     given:
-    def tempDir = File.createTempDir()
     rootProject.version = '1.1'
     rootProject.extensions['docker'].port 8082
-    rootProject.buildDir = tempDir
     rootProject.extensions.docker.dockerfile """FROM java:8
                                                 EXPOSE 9091
                                                 CMD echo 'ok'
     """
     when:
-    rootProject.getTasksByName(TASK_NAME, false)*.executeTask()
+    generateDockerfileTask.executeTask()
     then:
-    def dockerfile = new File(tempDir, 'docker/Dockerfile')
+    def dockerfile = new File(rootProject.buildDir, 'docker/Dockerfile')
     dockerfile.exists()
     dockerfile.getText('UTF-8') == '''FROM java:8
                                      |EXPOSE 9091
